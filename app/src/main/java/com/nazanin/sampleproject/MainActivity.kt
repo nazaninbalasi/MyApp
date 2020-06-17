95b4b9708a7a0cc5e505a19b0cc956b3dc5c3a9f
nazaninbalasi <balasinazanin@gmail.com>
2020-06-17 11:05:55 +0430

    first commit

---
+++ app/src/main/java/com/nazanin/sampleproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nazanin.sampleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
